abe2d954311c40c1c3c709522418dd7574f2b4db
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-07-09 01:44:20 +0900

    feat: create Area component

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -1,6 +1,18 @@
 import styled from '@components/style/Area.module.css';
-const Area = () => {
-  return <section className={styled.gameArea}></section>;
+import Bird from './Bird';
+interface AreaProps {
+  isActive: boolean;
+  areaRef: React.RefObject<HTMLDivElement>;
+  bodyRef: React.RefObject<HTMLDivElement>;
+  wingRef: React.RefObject<HTMLDivElement>;
+}
+
+const Area = ({ isActive, areaRef, bodyRef, wingRef }: AreaProps) => {
+  return (
+    <div className={styled.gameArea} ref={areaRef}>
+      <Bird isActive={isActive} bodyRef={bodyRef} wingRef={wingRef} />
+    </div>
+  );
 };
 
 export default Area;
